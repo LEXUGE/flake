898ee48c6fbdebe3b1bcb9bfbe2a577ddc5218e5
Harry Ying <lexugeyky@outlook.com>
2022-09-28 23:11:21 +0800

    buildiso: fix fonts in x1c7-img

--- cfgs/x1c7-img/default.nix
+++ cfgs/x1c7-img/default.nix
@@ -32,6 +32,15 @@
       };
     };
 
+    # Fonts
+    fonts.fonts = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      fira-code
+      fira-code-symbols
+    ];
+
     isoImage.edition = "gnome";
 
     # Whitelist wheel users to do anything
