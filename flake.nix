88a5400230f2a0a404721f2df26fe6f21e5ee5a4
Harry Ying <lexugeyky@outlook.com>
2022-11-26 09:16:14 +0000

    flake: sync hm upstream & enable grub.copyKernels

--- flake.nix
+++ flake.nix
@@ -19,7 +19,8 @@
     # Home manager
     # Broken due to https://github.com/nix-community/home-manager/pull/3405
     # home-manager.url = "github:nix-community/home-manager";
-    home-manager.url = "github:NickCao/home-manager";
+    # home-manager.url = "github:NickCao/home-manager";
+    home-manager.url = "github:LEXUGE/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Secrets management
