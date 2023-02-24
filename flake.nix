67ad98c4e0114ab25e30bc042ff0c6a45e889cd9
Harry Ying <lexugeyky@outlook.com>
2023-02-24 14:07:12 +0000

    build: fix agenix in LiveCD

--- flake.nix
+++ flake.nix
@@ -43,7 +43,8 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Secrets management
-    agenix.url = "github:ryantm/agenix";
+    # Agenix breaks in LiveCD due to https://github.com/ryantm/agenix/issues/165.
+    agenix.url = "github:ryantm/agenix/d7fd31756e1c5f1281981c48efbb2e188024ba47";
     agenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
