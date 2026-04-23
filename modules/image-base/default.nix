30b1b620035f0cfd19a5c9b903c9f84c213daef5
Harry Ying <lexugeyky@outlook.com>
2026-04-23 17:12:14 +0100

    build: update action

--- modules/image-base/default.nix
+++ modules/image-base/default.nix
@@ -69,7 +69,7 @@ in
     fonts.packages = with pkgs; [
       noto-fonts
       noto-fonts-cjk-sans
-      noto-fonts-emoji
+      noto-fonts-color-emoji
       fira-code
       fira-code-symbols
       nerd-fonts.fira-code
